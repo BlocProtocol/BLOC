ab2d33d64d01d6d3c4955cd0df2432205d094f55
L5K <65515026+cryptoL5k@users.noreply.github.com>
2022-01-22 21:42:12 +0000

    Update WalletConfig.h

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -37,7 +37,7 @@ namespace WalletConfig
     const std::string coinName = std::string(CryptoNote::CRYPTONOTE_NAME);
 
     /* Where can your users contact you for support? E.g. discord */
-    const std::string contactLink = "https://bloc.money";
+    const std::string contactLink = "https://blocprotocol.network/";
 
 
     /* The number of decimals your coin has */
